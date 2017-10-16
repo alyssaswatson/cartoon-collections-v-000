716a44f68f94aed2f8645582ca0f368d97344aa8
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-16 22:29:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,9 @@
 def roll_call_dwarves(dwarves)
   i = 0
-  new_dwarves = []
   while i < dwarves.length
-    new_dwarves << yield.each_with_index
+    prints "#{dwarves.each_with_index}"
     i += 1
   end
-  new_dwarves
 end
 
 def summon_captain_planet# code an argument here
