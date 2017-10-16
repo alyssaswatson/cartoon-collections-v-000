987eb19695c669f8a420ddee8193fe250057f6de
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-16 23:00:26 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,5 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find {|i| i.include? cheese_types(i)}
+  array.find {|i| cheese_types.include? i }
 end
