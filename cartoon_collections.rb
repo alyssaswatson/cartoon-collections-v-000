a58264b3e612a16715c0665c5117bf5c081c2104
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-16 22:22:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
-def roll_call_dwarves(names)
+def roll_call_dwarves(dwarves)
   i = 0
-  while 1 < names.length
-    puts names.each_with_index
+  while 1 < dwarves.length
+    puts dwarves.each_with_index
     i += 1
   end
 end
