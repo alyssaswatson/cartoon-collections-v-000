307c120a01541e2ade68905de081c12c86702f36
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-16 22:48:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |dwarf, index|
     puts "/#{index+1}.*#{dwarf}/"
@@ -15,8 +13,6 @@ end
 def long_planeteer_calls(calls)
   calls.include? {|i| i.length > 4}
 end
-
-binding.pry
 
 def find_the_cheese# code an argument here
   # the array below is here to help
