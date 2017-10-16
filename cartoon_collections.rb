5aebe09f11622912445534b47ca68bec82d763c0
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-16 22:46:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |dwarf, index|
@@ -15,7 +15,7 @@ end
 def long_planeteer_calls(calls)
   calls.include? {|i| i.length > 4}
 end
-
+binding.pry
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
